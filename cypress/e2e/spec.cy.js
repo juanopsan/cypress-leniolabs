42abbd8133163a82994004356569724a6fd6bdec
Juan Pablo Sanchez <juanop.sanchez@gmail.com>
2024-04-03 16:23:05 -0300

    login test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('login suite', () => {
+  beforeEach(() => {
+    cy.visit('auth/login')
+  })
+
+  it('should login successfully with valid credentials', () => {
+    cy.get('input[name=username]')
+      .clear()
+      .type('Admin')
+
+    cy.get('input[name=password]')
+      .clear()
+      .type('admin123')
+
+    cy.get('button[type=submit]')
+      .should('be.visible')
+      .click()
+
+    cy.get('h6')
+      .should('have.text', 'Dashboard')
   })
 })
